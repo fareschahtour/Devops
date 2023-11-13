26289467ee4caa2311225db15bb62a61df0a2de1
fareschahtour <fares.chahtour@esprit.tn>
2023-11-13 01:39:23 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/tn/esprit/spring/GestionDevOps_ProjectTests.java
@@ -0,0 +1,14 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDevOps_ProjectTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
